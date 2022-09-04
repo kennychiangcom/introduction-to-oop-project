5d37df2c5cdcff723607e3ac774ae429ca309840
kennychiangcom <kenny@chiang.com>
2022-09-04 21:32:49 +0100

    Add project files.

---
+++ introduction-to-oop-project/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using introduction_to_oop_project;
+
+//Console.WriteLine("Hello, World!");
+Bird myBird = new Bird("Tweety", "Yellow");
+myBird.Speak();
+myBird.Fly();
+Pigeon pippa = new Pigeon("Pippa", "Grey");
+pippa.Speak();
+pippa.Fly();
+pippa.EatPizza();
+pippa.Sleep();
