d3e18b289ddfd5c7e376823ac1966efe6c4a5ca0
kennychiangcom <kenny@chiang.com>
2022-09-04 22:02:50 +0100

    Exercise 4

--- introduction-to-oop-project/Pigeon.cs
+++ introduction-to-oop-project/Pigeon.cs
@@ -15,6 +15,11 @@ namespace introduction_to_oop_project
         {
         }
 
+        public override void Speak()
+        {
+            Console.WriteLine($"Yo! I'm {Name} and I'm a {Color} pigeon.");
+        }
+
         public void EatPizza()
         {
             Console.WriteLine("Delicious pizza!");
--- introduction-to-oop-project/Program.cs
+++ introduction-to-oop-project/Program.cs
@@ -2,11 +2,16 @@
 using introduction_to_oop_project;
 
 //Console.WriteLine("Hello, World!");
-Bird myBird = new Bird("Tweety", "Yellow");
-myBird.Speak();
-myBird.Fly();
+//Bird myBird = new Bird("Tweety", "Yellow");
+//myBird.Speak();
+//myBird.Fly();
 Pigeon pippa = new Pigeon("Pippa", "Grey");
 pippa.Speak();
 pippa.Fly();
 pippa.EatPizza();
 pippa.Sleep();
+Penguin pingu = new Penguin("Pingu", "Black & White");
+pingu.Speak();
+pingu.Fly();
+Parrot kaboo = new Parrot("Kaboo", "Red & Blue");
+kaboo.Speak();
